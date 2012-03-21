776e55ee0108ac8d7268bee79940d6bf299733a4
Jonathan Foerster <jonathan.foerster@gmail.com>
2012-03-20 21:33:06 -0700

    path smoothing now works.

--- path_smoothing.py
+++ path_smoothing.py
@@ -32,14 +32,23 @@ path = [[0, 0],
 # smooth coordinates
 #
 
-def smooth(path, weight_data = 0.5, weight_smooth = 0.1):
+def smooth(path, weight_data = 0.5, weight_smooth = 0.1, tolerance = 0.0000001):
 
     # Make a deep copy of path into newpath
     newpath = [[0 for row in range(len(path[0]))] for col in range(len(path))]
     for i in range(len(path)):
         for j in range(len(path[0])):
             newpath[i][j] = path[i][j]
-
+    
+    change = tolerance
+    while change >= tolerance:
+        change = 0.0
+        for i in range(1, len(path)-1):
+            for j in range(len(path[0])):
+                aux = newpath[i][j]
+                newpath[i][j] += weight_data * (path[i][j] - newpath[i][j])
+                newpath[i][j] += weight_smooth * (newpath[i+1][j] + newpath[i-1][j] - (2.0 * newpath[i][j]))
+                change += abs(aux - newpath[i][j])
 
     #### ENTER CODE BELOW THIS LINE ###
     
@@ -53,8 +62,3 @@ newpath = smooth(path)
 # thank you - EnTerr - for posting this on our discussion forum
 for i in range(len(path)):
     print '['+ ', '.join('%.3f'%x for x in path[i]) +'] -> ['+ ', '.join('%.3f'%x for x in newpath[i]) +']'
-
-
-
-
-
