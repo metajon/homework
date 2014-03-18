877dec1bbd266f8988b1de915ff8517d26f138d3
Jonathan Foerster <jonathan.foerster@gmail.com>
2014-03-17 21:29:01 -0700

    works as expected.  ready to refactor

--- extra/recipe_randomizer.py
+++ extra/recipe_randomizer.py
@@ -4,16 +4,21 @@ yeast = ['English Ale Yeast','American Ale Yeast','Belgian ALe Yeast','Californi
 base_malt = ['Maris Otter','Vienna Malt','2-Row']
 specialty_malt =['Oats','Flaked Barley','Un-malted wheat']
 hops = {'Bittering Hops' : ['Cascade','Chinook','East Kent Golding','Willamette','Mt. Hood','Challenger'], 'Flavoring Hops' : ['Simcoe','East Kent Golding','Amarillo','None'], 'Aroma Hops' : ['Falconer\'s Flight','East Kent Golding','Amarillo','None']}
+hops_qty = ['1/2 oz.','1 oz.']
 hop_schedule = ['60 minutes','30 minutes','15 minutes','Flameout']
 extract_qty = ['5#','5.5#','6#']
 #
-# print yeast[(randint(0,(len(yeast)-1)))]
+def specialty_grains(specialty_malt):
+	my_specialty_grains = []
+	for i in range(randint(0,2)):
+		my_specialty_grains.append(specialty_malt[(randint(0,(len(specialty_malt)-1)))])
+	if len(my_specialty_grains) > len(set(my_specialty_grains)):
+		print '1 pound ' + my_specialty_grains[0]
+	else:
+		for i in range(len(my_specialty_grains)):
+			print '1/2 pound ' + my_specialty_grains[i]
 #
-#all_ingredients = [yeast,base_malt,specialty_malt,bittering_hops,flavoring_aroma_hops,extract_qty]
-ingredients = [yeast,base_malt,specialty_malt,extract_qty]
-#
-#for i in range(len(all_ingredients)):
-#	print all_ingredients[i][(randint(0,(len(all_ingredients[i])-1)))]
+ingredients = [yeast,base_malt,extract_qty]
 #
 def make_recipe(ingredients):
 	for i in range(len(ingredients)):
@@ -22,11 +27,22 @@ def make_recipe(ingredients):
 def gen_hop_schedule(hops,hop_schedule):
 	for i in range(len(hop_schedule)):
 		if hop_schedule[i] == '60 minutes':
-			print hop_schedule[i] + ': ' + hops['Bittering Hops'][(randint(0,(len( hops['Bittering Hops'] )-1)))]
+			print hop_schedule[i] + ': ' + hops_qty[(randint(0,(len(hops_qty)-1)))] + ' ' + hops['Bittering Hops'][(randint(0,(len( hops['Bittering Hops'] )-1)))]
 		elif hop_schedule[i] == 'Flameout':
-			print hop_schedule[i] + ': ' + hops['Aroma Hops'][(randint(0,(len( hops['Aroma Hops'] )-1)))]
+			this_hop = hops['Aroma Hops'][(randint(0,(len( hops['Aroma Hops'] )-1)))]
+			if this_hop != 'None':
+				this_qty = hops_qty[(randint(0,(len(hops_qty)-1)))]
+			else:
+				this_qty = ''
+			print hop_schedule[i] + ': ' + this_qty + ' ' + this_hop
 		else:
-			print hop_schedule[i] + ': ' + hops['Flavoring Hops'][(randint(0,(len( hops['Flavoring Hops'] )-1)))]
+			this_hop = hops['Aroma Hops'][(randint(0,(len( hops['Aroma Hops'] )-1)))]
+			if this_hop != 'None':
+				this_qty = hops_qty[(randint(0,(len(hops_qty)-1)))]
+			else:
+				this_qty = ''
+			print hop_schedule[i] + ': ' + this_qty + ' ' + this_hop
 #	
 make_recipe(ingredients)
+specialty_grains(specialty_malt)
 gen_hop_schedule(hops,hop_schedule)
