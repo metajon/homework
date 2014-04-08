c7ebd5d9e7ab0ed7b823515e3c59e9e8eb15d395
Jonathan Foerster <jonathan.foerster@gmail.com>
2014-04-07 22:04:52 -0700

    just stopping for now

--- extra/read2me/r2me_lib.py
+++ extra/read2me/r2me_lib.py
@@ -1,11 +1,23 @@
 import feedparser
 from bs4 import BeautifulSoup
-url = ['http://www.therobotreport.com/feed']
-d = feedparser.parse(url[0])
+urls = {'The Robot Report':'http://www.therobotreport.com/feed'}
+d = feedparser.parse(urls['The Robot Report'])
+#
+#if 'content' in d['entries'][0].keys():
+#	c = d['entries'][0]['content']
+#else:
+#	c = d['entries'][0]['summary']
+#soup = BeautifulSoup(str(c))
+#print(soup.get_text())
+def show_feeds(urls):
+    print(urls.keys())
 
-if 'content' in d['entries'][0].keys():
-    c = d['entries'][0]['content']
-else:
-    c = d['entries'][0]['summary']
-soup = BeautifulSoup(str(c))
-print(soup.get_text())
+def show_titles(d):
+    for i in range(len(d)):
+        print('[' + str(i) + '] ' + d['entries'][i]['title'])
+
+def request_content(article_list):
+    if type(article_list) is not list:
+        print('Function requires a list.')
+    else:
+        pass
