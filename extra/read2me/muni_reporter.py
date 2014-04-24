dfcc42d362d1c7637784b23e26356f2be00dfd2b
Jonathan Foerster <jonathan.foerster@gmail.com>
2014-04-23 20:04:52 -0700

    minor corrections, and a test function

--- extra/read2me/muni_reporter.py
+++ extra/read2me/muni_reporter.py
@@ -10,9 +10,15 @@ def show_feeds(urls):
     print(urls.keys())
 
 def parse_url(url):
-	parsed_feed = d = feedparser.parse(url)
+	parsed_feed = feedparser.parse(url)
 	return parsed_feed
 
 def print_titles(parsed_feed):
-    for i in range(len(parsed_feed)):
+    for i in range(len(parsed_feed['entries'])):
         print('[' + str(i) + '] ' + parsed_feed['entries'][i]['title'])
+
+def print_all_titles(urls):
+	for i in range(len(urls)):
+		parsed_feed = parse_url(urls.values()[i])
+		for j in range(len(parsed_feed['entries'])):
+			print('[' + str(j) + '] ' + parsed_feed['entries'][j]['title'])
